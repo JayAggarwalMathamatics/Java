fdd02d95d37be7be46bd502536adf037ef4d8ea0
Jay Aggarwal <jayaggarwal14@gmail.com>
2025-07-24 18:23:43 -0700

    Add files via upload

---
+++ j1.java
@@ -0,0 +1,18 @@
+package jdemo1;
+
+// you can do: class Main{}, and no packages
+public class j1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int maths,sci,eng,total,avg;
+		maths=20;
+		sci=30;
+		eng=40;
+		total=maths+sci+eng;
+		avg=total/3;
+		System.out.println("Total = " + total);
+		System.out.println("Average = " + avg);
+		
+	}
+}
---
+++ j2.java
@@ -0,0 +1,19 @@
+package jdemo1;
+
+public class j2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d1,d2,d3,d4,d5,sum,avg;
+		d1=62;
+		d2=67;
+		d3=71;
+		d4=74;
+		d5=69;
+		sum=d1+d2+d3+d4+d5;
+		avg=sum/5;
+		System.out.println("Sum:" + sum);
+		System.out.println("Average:" + avg);
+	}
+
+}
